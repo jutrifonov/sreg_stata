91809d19a32143306787ba10180e723ae59470c6
Juri Trifonov <jutrifonov@uchicago.edu>
2024-08-12 09:07:07 -0500

    Update sreg_script.py

--- ado/sreg_script.py
+++ ado/sreg_script.py
@@ -2,14 +2,7 @@ import pandas as pd
 import numpy as np
 from sreg import sreg # Ensure these imports work in your Python environment
 
-print("sreg_script is being imported")
-
-#def run_sreg(Y=None, S=None, D=None, G_id=None, Ng=None, X=None, HC1=True):
-#    result = sreg(Y=Y, S=S, D=D, G_id=G_id, Ng=Ng, X=X, HC1=HC1)
-#    return result
 def run_sreg(Y=None, S=None, D=None, G_id=None, Ng=None, X=None, HC1=True):
-    print('WE ARE HERE!')
-    # Convert inputs to NumPy arrays if they are not None
     if Y is not None:
         Y = np.array(Y)
     if S is not None:
@@ -21,6 +14,6 @@ def run_sreg(Y=None, S=None, D=None, G_id=None, Ng=None, X=None, HC1=True):
     if Ng is not None:
         Ng = np.array(Ng)
     if X is not None:
-        X = pd.DataFrame(X)  # Assuming X is passed as a dictionary of lists
+        X = pd.DataFrame(X)
     result = sreg(Y=Y, S=S, D=D, G_id=G_id, Ng=Ng, X=X, HC1=HC1)
     return result
